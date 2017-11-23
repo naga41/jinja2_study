1b4361fe1bf910564c78cbb61042631e7cda98e0
Shuhei Nagata <shnagata.41@gmail.com>
2017-11-23 23:40:28 +0900

    Print templates' string with param.

--- index.py
+++ index.py
@@ -1,4 +1,26 @@
-from jinja2 import Template
+# -*- coding: utf-8 -*-
+import yaml
 
-template = Template('Hello {{ name }}!')
-print template.render(name='John Doe')
+import jinja2
+
+TEMPLATE_DIR = './templates'
+PARAMS='./app.yml'
+
+
+def load_template_params(parameter_file):
+    with open(parameter_file) as f:
+        params = yaml.load(f)
+    return params
+
+
+def build_templates(template_dir, dict_vars):
+
+    template_loader = jinja2.FileSystemLoader(template_dir)
+    template_env = jinja2.Environment(loader=template_loader)
+
+    for template in template_env.list_templates(extensions='.j2'):
+        print template_env.get_template(template).render(dict_vars)
+
+if __name__ == "__main__":
+    template_params = load_template_params(PARAMS)
+    build_templates(TEMPLATE_DIR, template_params)
